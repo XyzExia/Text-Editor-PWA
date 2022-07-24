215d5b0c68dfb35125449af5fa235811402218ef
Zekun Chen <zekunchen97@gmail.com>
2022-07-24 22:29:32 +0930

    make it better

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,22 +3,22 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// * TODO: Add and configure workbox plugins for a service worker and manifest file.
+// * Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
+	return {
+		mode: 'development',
+		entry: {
+			main: './src/js/index.js',
+			install: './src/js/install.js',
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
 				template: './index.html',
 			}),
 			new WebpackPwaManifest({
@@ -43,13 +43,12 @@ module.exports = () => {
 			new InjectManifest({
 				swSrc: './src-sw.js',
 				swDest: 'service-worker.js',
-			})
-      
-    ],
+			}),
+		],
 
-    module: {
-      rules: [
-        {
+		module: {
+			rules: [
+				{
 					test: /\.css$/i,
 					use: ['style-loader', 'css-loader'],
 				},
@@ -66,8 +65,8 @@ module.exports = () => {
 							],
 						},
 					},
-				}
-      ],
-    },
-  };
+				},
+			],
+		},
+	};
 };
