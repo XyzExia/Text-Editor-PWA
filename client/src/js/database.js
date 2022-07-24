dc1a0aa0516af67e6aefedc9b259a0c940677933
Zekun Chen <zekunchen97@gmail.com>
2022-07-24 22:21:56 +0930

    testing 1

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,18 +1,22 @@
 import { openDB } from 'idb';
 
+// Create database
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', {
+				keyPath: 'id',
+				autoIncrement: true,
+			});
+			console.log('jate database created');
+		},
+	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add text to indexedDB
 export const putDb = async (content) => {
 	const jateDB = await openDB('jate', 1);
 	const tx = jateDB.transaction('jate', 'readwrite');
@@ -22,7 +26,7 @@ export const putDb = async (content) => {
 	console.log(result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Retrieve text from indexedDB
 export const getDb = async (e) => {
 	const jateDb = await openDB('jate', 1);
 	const tx = jateDb.transaction('jate', 'readonly');
